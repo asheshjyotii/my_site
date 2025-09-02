569c1cd93d180b411aee1c3acbc8666171459a8f
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2025-09-02 05:45:50 +0530

    css and domain ssl problem

--- main_site/main_site/settings.py
+++ main_site/main_site/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # separate folder for collected files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 #Compressor settings
 COMPRESS_ROOT = BASE_DIR / 'static'
